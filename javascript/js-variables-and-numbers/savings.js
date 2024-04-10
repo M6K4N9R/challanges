8c04b16ba9c84f68e410f06a3ab20bb28675b96b
M6K4N9R <zubchevskiy@gmail.com>
2024-04-10 12:30:39 +0200

    Add total savings

---
+++ javascript/js-variables-and-numbers/savings.js
@@ -0,0 +1,7 @@
+let startingBalance = 45000;
+let monthlyContribution = 500;
+let months = 12;
+
+let totalSavings = startingBalance + (monthlyContribution * months);
+
+console.log('After', months, 'months of saving, you will have', totalSavings, 'in your account.');
