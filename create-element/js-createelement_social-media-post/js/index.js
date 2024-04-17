4b54ae264cbae991f576883aa480cb2b30ec89f7
M6K4N9R <zubchevskiy@gmail.com>
2024-04-17 12:43:29 +0200

    Add all other elements

--- create-element/js-createelement_social-media-post/js/index.js
+++ create-element/js-createelement_social-media-post/js/index.js
@@ -12,6 +12,22 @@ newParagraph.innerHTML =
   "Hey there! Please disregard that I use innerHtml.</br>I want to try out different things <strong>bigger</strong> and beeing able to jump to the next line, that's all.";
 newPost.append(newParagraph);
 
+const newFooter = document.createElement("footer");
+newFooter.classList.add("post__footer");
+newPost.append(newFooter);
+
+const newSpan = document.createElement("span");
+newSpan.classList.add("post__username");
+newSpan.textContent = "@username";
+newFooter.append(newSpan);
+
+const newLike = document.createElement("button");
+newLike.classList.add("post__button");
+newLike.setAttribute("type", "button");
+newLike.dataset.js = "like-button";
+newLike.textContent = "♥ Like";
+newFooter.append(newLike);
+
 console.log(newPost);
 
 function handleLikeButtonClick(event) {
