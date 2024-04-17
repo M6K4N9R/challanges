f6eedf5ff95699ffb907623e1609daefcc5f5667
M6K4N9R <zubchevskiy@gmail.com>
2024-04-17 11:29:06 +0200

    Add poping new toast message

--- create-element/js-createelement_toast-messages/js/index.js
+++ create-element/js-createelement_toast-messages/js/index.js
@@ -3,15 +3,17 @@ console.clear();
 const toastContainer = document.querySelector('[data-js="toast-container"]');
 const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
-const newToastMessage = document.createElement('li');
-newToastMessage.textContent = "...and I am a next Happy Toast Message"
+const newToastMessage = document.createElement("li");
+newToastMessage.textContent = "...and I am a next Happy Toast Message";
+newToastMessage.classList.add("toast-container__message");
 
 addButton.addEventListener("click", () => {
   // Exercise: Append a new entry to the toast messages container
+  toastContainer.append(newToastMessage);
 });
 
 clearButton.addEventListener("click", () => {
   // Exercise: Clear the stack of toast messages
 });
 
-console.log()
+console.log(newToastMessage.textContent);
