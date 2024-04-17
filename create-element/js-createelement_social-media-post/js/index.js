805e348ebecb9d6d87c62493a6162cd906b78666
M6K4N9R <zubchevskiy@gmail.com>
2024-04-17 12:20:17 +0200

    Create and append new section to body

--- create-element/js-createelement_social-media-post/js/index.js
+++ create-element/js-createelement_social-media-post/js/index.js
@@ -1,5 +1,13 @@
 console.clear();
 
+const body = document.getElementsByTagName("body");
+
+const newPost = document.createElement("section");
+newPost.classList.add("post");
+document.body.append(newPost);
+
+console.log(newPost);
+
 function handleLikeButtonClick(event) {
   const buttonElement = event.target;
   buttonElement.classList.toggle("post__button--liked");
