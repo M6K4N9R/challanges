7bcae6dfdc45e7322a0bce61cbb829d7f987e701
M6K4N9R <zubchevskiy@gmail.com>
2024-04-17 11:58:44 +0200

    Done add message functionality

--- create-element/js-createelement_toast-messages/js/index.js
+++ create-element/js-createelement_toast-messages/js/index.js
@@ -3,13 +3,23 @@ console.clear();
 const toastContainer = document.querySelector('[data-js="toast-container"]');
 const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
-const newToastMessage = document.createElement("li");
-newToastMessage.textContent = "...and I am a next Happy Toast Message";
-newToastMessage.classList.add("toast-container__message");
+
+let count = 0;
+
+function addMessage() {
+  const newToastMessage = document.createElement("li");
+  newToastMessage.textContent = "...and I'am a new Happy Toast Message!";
+  newToastMessage.classList.add("toast-container__message");
+  toastContainer.append(newToastMessage);
+}
+
+// const newToastMessageLast = document.createElement("li");
+// newToastMessageLast.textContent = "I am the last one, folk. It's over!";
+// newToastMessageLast.classList.add("toast-container__message");
 
 addButton.addEventListener("click", () => {
   // Exercise: Append a new entry to the toast messages container
-  toastContainer.append(newToastMessage);
+  addMessage();
 });
 
 clearButton.addEventListener("click", () => {
