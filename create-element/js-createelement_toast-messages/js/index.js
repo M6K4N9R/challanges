f333765dc2822a28d211e1de4b22947ae7b18f0b
M6K4N9R <zubchevskiy@gmail.com>
2024-04-17 11:25:10 +0200

    Create toast message variable and add text content

--- create-element/js-createelement_toast-messages/js/index.js
+++ create-element/js-createelement_toast-messages/js/index.js
@@ -3,6 +3,8 @@ console.clear();
 const toastContainer = document.querySelector('[data-js="toast-container"]');
 const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
+const newToastMessage = document.createElement('li');
+newToastMessage.textContent = "...and I am a next Happy Toast Message"
 
 addButton.addEventListener("click", () => {
   // Exercise: Append a new entry to the toast messages container
@@ -11,3 +13,5 @@ addButton.addEventListener("click", () => {
 clearButton.addEventListener("click", () => {
   // Exercise: Clear the stack of toast messages
 });
+
+console.log()
