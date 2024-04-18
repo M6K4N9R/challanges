500c0ddec9cafd4b5e6374457954c4d8653af844
M6K4N9R <zubchevskiy@gmail.com>
2024-04-18 11:40:15 +0200

    Does Task 1

--- ja-callback-functions/js-callback-functions_user-login-callback-2-with-argument/js/index.js
+++ ja-callback-functions/js-callback-functions_user-login-callback-2-with-argument/js/index.js
@@ -5,3 +5,14 @@ function handleUserLogin(onSuccess) {
 }
 
 // The exercise starts here!
+
+function showWelcomeMessage(userName) {
+  console.log(`Welcome ${userName} You are logged in now.`);
+}
+
+const showMessage = (userName) => {
+  console.log(`Welcome ${userName} You are logged in now.`);
+};
+
+handleUserLogin(showWelcomeMessage);
+handleUserLogin(showMessage);
