9088a5c5b1e9001e52ea13d65c55e28392e1fb09
M6K4N9R <zubchevskiy@gmail.com>
2024-04-23 11:32:14 +0200

    App is working

---
+++ linting-basics-template/index.js
@@ -0,0 +1,16 @@
+const cards = document.querySelectorAll('[data-js="card"]');
+const buttons = document.querySelectorAll('[data-js="button"]');
+
+function toggleButton() {
+  cards.forEach(card => {
+    const button = card.querySelector('[data-js="button"]');
+    const link = card.querySelector('[data-js="link"]');
+    const arrow = card.querySelector('[data-js="arrow"]');
+    button.addEventListener('click', () => {
+      link.classList.toggle('hidden');
+      arrow.classList.toggle('rotate');
+    });
+  });
+}
+
+toggleButton();
