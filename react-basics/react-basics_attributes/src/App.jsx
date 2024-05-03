1b2933a50c6be3eb596cdf0178ec02b602558325
M6K4N9R <zubchevskiy@gmail.com>
2024-05-03 16:12:07 +0200

    Finish the anchor

--- react-basics/react-basics_attributes/src/App.jsx
+++ react-basics/react-basics_attributes/src/App.jsx
@@ -3,3 +3,12 @@ import "./App.css";
 export default function App() {
   return <div>Replace me with an article</div>;
 }
+
+function Article() {
+  <article class="article">
+    <h2 className="article__title">Yo, What's UP!</h2>
+    <label for="question">What is your problem?</label>
+    <input type="textarea" name="user-input" id="question" cols="30" rows="10"></input>
+    <a className="article__link" ></a>
+  </article>
+}
