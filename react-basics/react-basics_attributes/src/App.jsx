b66cc86d4441b34e83d60265292186eafce3818b
M6K4N9R <zubchevskiy@gmail.com>
2024-05-03 17:10:51 +0200

    done

--- react-basics/react-basics_attributes/src/App.jsx
+++ react-basics/react-basics_attributes/src/App.jsx
@@ -7,8 +7,8 @@ export default function App() {
 function Article() {
   <article class="article">
     <h2 className="article__title">Yo, What's UP!</h2>
-    <label for="question">What is your problem?</label>
+    <label htmlFor="question">What is your problem?</label>
     <input type="textarea" name="user-input" id="question" cols="30" rows="10"></input>
-    <a className="article__link" ></a>
+    <a className="article__link" href="https://de.wikipedia.org/wiki/Problem">Find your answer</a>
   </article>
 }
