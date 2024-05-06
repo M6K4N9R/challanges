082e9b3736e5eaf79aa96c516c8e256080dfda27
M6K4N9R <zubchevskiy@gmail.com>
2024-05-06 11:22:25 +0200

    Create Sum component

--- react-props/react-props_sum/src/App.jsx
+++ react-props/react-props_sum/src/App.jsx
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return (
+    <section className="sum-function">
+      <Sum valueA={23} valueB={45} />
+    </section>
+  );
+}
+
+function Sum({ valueA, valueB }) {
+  let outcome = valueA + valueB;
+  console.log(outcome);
+  return (
+    <h1>
+      {valueA} + {valueB} = outcome;
+    </h1>
+  );
 }
