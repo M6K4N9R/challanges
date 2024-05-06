241fd8cf100d6616c494f0dae1bb06d3ba626f5c
M6K4N9R <zubchevskiy@gmail.com>
2024-05-06 13:50:51 +0200

    task two done

--- react-props/react-props_button/src/App.jsx
+++ react-props/react-props_button/src/App.jsx
@@ -1,5 +1,26 @@
 import "./App.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return (
+    <h1>
+      <Button text="Click Me" disabled={false} color={"#DBABBE"} />
+    </h1>
+  );
+}
+
+function Button({ color, disabled, text }) {
+  return (
+    <button
+      style={{
+        color: color,
+        backgroundColor: "#797394",
+        padding: "5px 15px",
+        fontSize: "21px",
+        borderRadius: "9px",
+      }}
+      onClick={() => alert("Ah! Don't touch me.")}
+    >
+      {text} {disabled}
+    </button>
+  );
 }
