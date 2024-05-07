693d4ddb1d2d01e8b4f2bd28ed0c31e2716e5804
M6K4N9R <zubchevskiy@gmail.com>
2024-05-07 17:49:35 +0200

    Add Activate and Deactivate feature

--- react-state/react-state_box/src/App.jsx
+++ react-state/react-state_box/src/App.jsx
@@ -5,15 +5,17 @@ export default function App() {
   const [active, setActive] = useState(false);
 
   function handleClick() {
-    setActive(!isActive);
+    setActive(!active);
     // Check that the value changes correctly.
-    console.log(isActive);
+    console.log(active);
   }
 
   return (
     <main>
-      <div className={`box ${isActive ? "box--active" : ""}`} />
-      <button onClick={handleClick}>Activate</button>
+      <div className={`box ${active ? "box--active" : ""}`} />
+      <button onClick={handleClick}>
+        {!active ? "Activate" : "Deactivate"}
+      </button>
     </main>
   );
 }
