0f1c0cb73b221a9be3abfb182a59fd8a06553e51
M6K4N9R <zubchevskiy@gmail.com>
2024-05-07 13:07:43 +0200

    Add article and align button to center

--- react-project-setup/journal-app/src/App.jsx
+++ react-project-setup/journal-app/src/App.jsx
@@ -28,13 +28,7 @@ function EntryForm() {
         <label for="input-title">Motto</label>
         <input type="text" id="input-title" name="moto" required />
         <label for="input-notes">Notes</label>
-        <textarea
-          id="input-notes"
-          name="notes"
-          rows="5"
-          cols="40"
-          required
-        ></textarea>
+        <textarea id="input-notes" name="notes" rows="10" required></textarea>
         <Button />
       </form>
     </>
@@ -42,12 +36,17 @@ function EntryForm() {
 }
 
 function EntriesSection() {
-  return <Tabs />;
+  return (
+    <>
+      <Tabs />
+      <EntriesList />
+    </>
+  );
 }
 
 function Tabs() {
   return (
-    <div className="tabs-section">
+    <section className="tabs-section">
       <div className="tabs tab--one">
         <h3>All Entries</h3>
         <p className="number-of-entries">
@@ -60,11 +59,45 @@ function Tabs() {
           <span>1</span>
         </p>
       </div>
-    </div>
+    </section>
   );
 }
 
-function EntriesList() {}
+function EntriesList() {
+  return (
+    <section className="entries-section">
+      <acticle className="entries --entry-one">
+        <div className="date">Feb 27 2024</div>
+        <div className="acticle-info">
+          <h3 className="entries--title">"That's Life in the city"</h3>
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M12 2L15.09 8.26L22 9.27L17 14.14L18.18 21.02L12 17.77L5.82 21.02L7 14.14L2 9.27L8.91 8.26L12 2Z"
+              fill="#FF4A11"
+              stroke="#FF4A11"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        </div>
+
+        <p className="article">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio, quos
+          corporis quo vero delectus ratione, libero quam voluptate magni
+          obcaecati natus architecto nulla veritatis! Commodi placeat quaerat
+          enim aliquid velit?
+        </p>
+      </acticle>
+    </section>
+  );
+}
 
 function Button() {
   return <input type="submit" className="form-button" value="Create" />;
