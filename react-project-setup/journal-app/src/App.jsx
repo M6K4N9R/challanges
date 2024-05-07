264ac6eecf4a7c494098c3c221d28e4029bdb519
M6K4N9R <zubchevskiy@gmail.com>
2024-05-07 15:21:24 +0200

    Generate another 3 articles

--- react-project-setup/journal-app/src/App.jsx
+++ react-project-setup/journal-app/src/App.jsx
@@ -56,6 +56,32 @@ function EntriesSection() {
           />
         }
       />
+      <EntriesList
+        title={"Another day in the village"}
+        date={"30 Feb - 2024"}
+        icon={
+          <path
+            d="M12 2L15.09 8.26L22 9.27L17 14.14L18.18 21.02L12 17.77L5.82 21.02L7 14.14L2 9.27L8.91 8.26L12 2Z"
+            stroke="#ff4a11"
+            stroke-width="1"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        }
+      />
+      <EntriesList
+        title={"Holiday in London"}
+        date={"14 Mar - 2024"}
+        icon={
+          <path
+            d="M12 2L15.09 8.26L22 9.27L17 14.14L18.18 21.02L12 17.77L5.82 21.02L7 14.14L2 9.27L8.91 8.26L12 2Z"
+            stroke="#ff4a11"
+            stroke-width="1"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        }
+      />
     </>
   );
 }
