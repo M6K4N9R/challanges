8aa050c864b403d575dfad06a5ef919834ac264e
M6K4N9R <zubchevskiy@gmail.com>
2024-05-10 10:35:06 +0200

    Render the fruits

--- react-with-arrays/react-with-arrays_fruits/src/App.jsx
+++ react-with-arrays/react-with-arrays_fruits/src/App.jsx
@@ -1,9 +1,7 @@
 import Card from "./components/Card";
 import "./App.css";
 
-
 export default function App() {
-  const fruits = [];
 
   return (
     <div className="app">
--- react-with-arrays/react-with-arrays_fruits/src/components/Card.jsx
+++ react-with-arrays/react-with-arrays_fruits/src/components/Card.jsx
@@ -1,5 +1,19 @@
 import "./Card.css";
 
 export default function Card({ name }) {
-  return <p className="card">{name}</p>;
+  const fruits = [
+    { id: 100, name: "🍌 Banana", texture: "soft", color: "yellow" },
+    { id: 101, name: "🍎 Green Apple", texture: "crunchy", color: "green" },
+    { id: 102, name: "🍑 Peach", texture: "juicy", color: "orange" },
+    { id: 103, name: "🍉 Watermelon", texture: "watery", color: "red" },
+  ];
+  return (
+    <section>
+      {fruits.map((fruit) => (
+        <p key={fruit.id} className="card">
+          {fruit.name}
+        </p>
+      ))}
+    </section>
+  );
 }
