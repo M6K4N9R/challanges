86fa11fd5a7c7b9e599248e6d8345c49f68cacbb
M6K4N9R <zubchevskiy@gmail.com>
2024-05-10 11:17:38 +0200

    Apply border

--- react-with-arrays/react-with-arrays_fruits/src/components/Card.jsx
+++ react-with-arrays/react-with-arrays_fruits/src/components/Card.jsx
@@ -1,19 +1,9 @@
 import "./Card.css";
 
-export default function Card({ name }) {
-  const fruits = [
-    { id: 100, name: "🍌 Banana", texture: "soft", color: "yellow" },
-    { id: 101, name: "🍎 Green Apple", texture: "crunchy", color: "green" },
-    { id: 102, name: "🍑 Peach", texture: "juicy", color: "orange" },
-    { id: 103, name: "🍉 Watermelon", texture: "watery", color: "red" },
-  ];
+export default function Card({ name, color }) {
   return (
-    <section>
-      {fruits.map((fruit) => (
-        <p key={fruit.id} className="card">
-          {fruit.name}
-        </p>
-      ))}
-    </section>
+    <p className="card" style={{ border: `3px solid ${color}` }}>
+      {name}
+    </p>
   );
 }
