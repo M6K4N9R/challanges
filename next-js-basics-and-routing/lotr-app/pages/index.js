7e78dd3f66e850d8ba50148b368b1dbce24c73dc
M6K4N9R <zubchevskiy@gmail.com>
2024-05-17 12:01:56 +0200

    Create folders

--- next-js-basics-and-routing/lotr-app/pages/index.js
+++ next-js-basics-and-routing/lotr-app/pages/index.js
@@ -1,7 +1,23 @@
+import Link from "next/link";
+import { introduction } from "../../lib/data";
+import { volumes } from "@/resources/lib/data";
+
 export default function HomePage() {
-  return (
-    <div>
-      <h1>Hello from Next.js</h1>
-    </div>
-  );
+  volumes.map((volume, i) => {
+    return (
+      <div>
+        <h1>Lord of The Ring App</h1>
+        <p>{introduction}</p>
+        <h2>All Volumes</h2>
+        <ul>
+          <li>
+            <Link href="./volumes/the-fellowship-of-the-ring">
+              {" "}
+              {volume.title}
+            </Link>
+          </li>
+        </ul>
+      </div>
+    );
+  });
 }
