bc0dbf63e2753c3418ff38658f88888d6f8e5efa
M6K4N9R <zubchevskiy@gmail.com>
2024-05-23 16:59:41 +0200

    MEssed up

--- react-project-setup/journal-app/src/components/EntriesSection.jsx
+++ react-project-setup/journal-app/src/components/EntriesSection.jsx
@@ -6,6 +6,9 @@ export default function EntriesSection({
   entries,
   onShowAllEntries,
   onShowFavoriteEntries,
+  onToggleFavorite,
+  isFavorite,
+  id,
 }) {
   return (
     <>
@@ -15,17 +18,21 @@ export default function EntriesSection({
           counter={3}
           className={"tabs__button tab--entries"}
           onClick={onShowFavoriteEntries}
-          
         />
         <Tab
           text={"Favorites"}
           counter={1}
           className={"tabs__button tab--favorites"}
           onClick={onShowAllEntries}
-          
         />
       </section>
-      <EntriesList icon={FavoriteButton} entries={entries} />
+      <EntriesList
+        icon={FavoriteButton}
+        entries={entries}
+        onToggleFavorite={onToggleFavorite}
+        isFavorite={isFavorite}
+        id={id}
+      />
     </>
   );
 }
